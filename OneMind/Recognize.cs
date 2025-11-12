3a77a3f77b21310379c9d5d5627ae3059e3d885b
apah6 <meng9940@naver.com>
2025-11-12 19:15:00 +0900

    road kinect

--- OneMind/Recognize.cs
+++ OneMind/Recognize.cs
@@ -1,23 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Kinect;
+using System;
+using System.Windows;
 
-using Microsoft.Kinect;
+public class Recognize //사용할때 Recognize recog = new Recognize();
 
-namespace OneMind
 {
-    internal class Recognize
-    {
-        //test
-        //umm..
-        //hmm..
-        public void readKinect()
-        {
+    private KinectSensor kinectsensor;
 
+    public KinectSensor Sensor //사용할때 KinectSensor sensor = recog.Sensor;
+    {
+        get { return kinectsensor; }
+    }
+
+    public Recognize()
+    {
+        InitializeKinect();
+    }
+
+    private void InitializeKinect()
+    {
+        try
+        {
+            var sensors = KinectSensor.KinectSensors;
+            if (sensors.Count > 0)
+            {
+                kinectsensor = sensors[0];
+                if (kinectsensor.Status == KinectStatus.Connected)
+                {
+                    kinectsensor.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Kinect가 연결되어 있지 않습니다.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Kinect 센서를 찾을 수 없습니다.");
+            }
         }
-        
+        catch (Exception ex)
+        {
+            MessageBox.Show("키넥트 초기화 오류입니다.: " + ex.Message);
+        }
+    }
+
+    public void CloseKinect()
+    {
+        if (kinectsensor != null && kinectsensor.IsRunning)
+        {
+            kinectsensor.Stop();
+        }
     }
 
 }
