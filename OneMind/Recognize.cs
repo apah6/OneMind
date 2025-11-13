cce31b3a73d907e7bfcd742dfea96b0f476041a7
apah6 <meng9940@naver.com>
2025-11-13 13:43:24 +0900

    initialize kinect

--- OneMind/Recognize.cs
+++ OneMind/Recognize.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Kinect;
 using System;
+using System.Linq;
 using System.Windows;
 
 public class Recognize //사용할때 Recognize recog = new Recognize();
@@ -19,10 +20,24 @@ public class Recognize //사용할때 Recognize recog = new Recognize();
 
     private void InitializeKinect()
     {
-        /*try
+        try
         {
-            //vdsdsdsd
-        }*/
+            kinectsensor = KinectSensor.KinectSensors.FirstOrDefault(s => s.Status == KinectStatus.Connected);
+
+            if (kinectsensor != null)
+            {
+                kinectsensor.Start();
+                MessageBox.Show("Kinect 연결 성공!");
+            }
+            else
+            {
+                MessageBox.Show("Kinect를 찾을 수 없습니다.");
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Kinect 초기화 중 오류 발생: " + ex.Message);
+        }
     }
 
     public void CloseKinect()
